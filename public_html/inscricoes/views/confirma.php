0df9a797839dc9562de68c0c28dad103f71610a3
Fernando Sandes <fernandosandes@infojr.com.br>
2012-05-18 20:04:10 -0300

    criando pasta para area administrativa

---
+++ public_html/inscricoes/views/confirma.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Inscrições CONAJE|ENEEJ</title>
+	<link   type="text/css" href="../css/index.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Arapey' rel='stylesheet' type='text/css'>
+	
+</head>
+
+<body>
+	
+	<div id="all">
+    	<div id="topo">
+        	<div id="logo"></div>
+        </div>
+        	
+           <div id="titulo"> <h1>Parabéns!</h1></div>
+           <div id="mensagem"><h2>Você fará parte do CONAJE & ENEEJ</h2></div>
+    </div><!-- div all -->     
+</body>
+
+</html>
